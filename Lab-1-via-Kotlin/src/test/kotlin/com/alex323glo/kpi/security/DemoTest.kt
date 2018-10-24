6d8e7a29d2e9e37f548e64eae025943b02d06ff6
Alexey Orlyuk <alex323glo@gmail.com>
2018-10-24 07:59:55 +0300

    Add Unit tests signatures.

---
+++ Lab-1-via-Kotlin/src/test/kotlin/com/alex323glo/kpi/security/DemoTest.kt
@@ -0,0 +1,12 @@
+package com.alex323glo.kpi.security
+
+import org.junit.Test
+
+class DemoTest {
+
+    @Test
+    fun mainDemoTest() {
+
+    }
+
+}
---
+++ Lab-1-via-Kotlin/src/test/kotlin/com/alex323glo/kpi/security/TableTest.kt
@@ -0,0 +1,15 @@
+package com.alex323glo.kpi.security
+
+import org.junit.Before
+import org.junit.Test
+
+class TableTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun generate() {
+    }
+}
